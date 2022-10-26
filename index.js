2dfaa4cdb12ed60d8aee55c187c387ac33c5752d
komkawila <you@example.com>
2022-10-26 13:26:04 +0700

    1

--- index.js
+++ index.js
@@ -16,16 +16,27 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+const connection = new ewelink({
+    email: 'devcm74@gmail.com',
+    password: 'P@ssw0rd',
+});
+
 app.get('/', async (req, res, next) => {
 
     (async () => {
-        const connection = new ewelink({
-            email: 'devcm74@gmail.com',
-            password: 'P@ssw0rd',
-        });
         const devices = await connection.getDevices();
         // console.log(devices);
         res.send(devices);
+    })();
+
+});
+
+app.get('/log/:deviceid', async (req, res, next) => {
+    (async () => {
+        const deviceid = await req.params.deviceid;
+        console.log(deviceid)
+        const usage = await connection.getDevicePowerUsage(deviceid);
+        res.send(usage);
     })();
 
 });
